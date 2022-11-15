dbab40f4ee7226ae56fd9902f91a6383ba60d533
Virajjogani <virajjogani205@gmail.com>
2022-11-15 09:30:32 +0530

    fixed the issue

--- controller/hotelcontroller.js
+++ controller/hotelcontroller.js
@@ -45,7 +45,7 @@ export const GetsingleHotels = async (req, res, next) => {
   }
 };
 export const CountBycity = async (req, res, next) => {
-  const cities = req.query.cities.split(",");
+  const cities = req.query.cities.split(',');
   try {
     const list = await Promise.all(
       cities.map((city) => {
